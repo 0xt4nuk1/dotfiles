1f948f9bdfab6b48a0293d4e2a346859c2ee1d05
Javier Parada <javierparadadev@gmail.com>
2024-07-17 22:13:16 +0200

    add casks

--- apps/macos/brew.sh
+++ apps/macos/brew.sh
@@ -4,3 +4,19 @@ brew install make
 brew install mas 
 brew install vim
 brew install git
+brew install docker
+
+brew install --cask firefox
+brew install --cask proton-pass
+brew install --cask iterm2
+brew install --cask telegram
+brew install --cask visual-studio-code
+brew install --cask proton-drive
+brew install --cask proton-mail
+brew install --cask protonvpn
+brew install --cask discord
+brew install --cask firefox@developer-edition
+brew install --cask steam
+brew install --cask keka
+brew install --cask mactex
+brew install --cask mongodb-compass
